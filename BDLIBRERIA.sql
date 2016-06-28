8db4135c227a25e091f587dafe789822521dbd78
Gloria <kcm3023@live.com>
2016-06-27 23:46:23 -0500

    DB nueva :)
    primer avance  aun me falta agregar mas cosas

--- BDLIBRERIA.sql
+++ BDLIBRERIA.sql
@@ -2,79 +2,18 @@ CREATE DATABASE Libreria
 GO
 
 USE Libreria
+Go
+
+CREATE TABLE Usuario(
+Usuario char(5) primary key,
+nick varchar(15) unique not null,
+Contrasena char(3)not null
+)
 GO
 
 CREATE TABLE TipoEmpleado(
 TipoEmpleadoID int Identity(1,1) not null primary key,
-Descripcion varchar(20) not null,
-Contrasena varchar(20) not null
-)
-GO
-
-CREATE TABLE Empleado(
-EmpleadoID int Identity(1,1) not null primary key,
-TipoEmpleadoID int not null,
-CodigoEmpleado varchar(20) unique not null,
-Nombre varchar(50) not null,
-ApellidoPaterno varchar(50) not null,
-ApellidoMaterno varchar(50) not null,
-Sueldo decimal(10,1) not null,
-Direccion varchar(150) not null,
-Telefono varchar(10) not null,
-DistritoID int not null
-)
-GO
-
-CREATE TABLE Distrito(
-DistritoID int Identity(1,1) not null primary key,
-NombreDist varchar(50),
-)
-GO
-
-CREATE TABLE Cliente(
-ClienteID int Identity(1,1) not null primary key,
-Nombre varchar(50) not null,
-ApellidoPaterno varchar(50) not null,
-ApellidoMaterno varchar(50) not null,
-Direccion varchar(150) not null,
-Telefono varchar(10) not null,
-TipoDocumentoID int not null,
-DistritoID int not null
-)
-GO
-
-CREATE TABLE TipoDocumento(
-TipoDocumentoID int Identity(1,1) not null primary key,
-Descripcion varchar(30) not null
-)
-GO
-
-CREATE TABLE Boleta(
-BoletaID int Identity(1,1) not null primary key,
-EmpleadoID int not null,
-ClienteID int not null
-)
-GO
-
-CREATE TABLE Producto(
-ProductoID int Identity(1,1) not null primary key,
-Nombre varchar(30) not null,
-MarcaID int not null,
-Precio decimal(5,2) not null,
-Stock int not null,
-Foto image,
-TipoProdId int not null
-)
-GO
-
-CREATE TABLE DetalleBoleta(
-ProductoID int not null,
-BoletaID int not null,
-IGV decimal(3,2) not null,
-Cantidad int not null,
-Precio decimal(5,2) not null,
-total decimal(9,2) not null,
-primary key (ProductoID,BoletaID)
+Descripcion varchar(20) not null
 )
 GO
 
@@ -86,383 +25,162 @@ Descripcion varchar(60) not null
 )
 GO
 
+CREATE TABLE TipoDocumento(
+TipoDocumentoID int Identity(1,1) not null primary key,
+Descripcion varchar(30) not null
+)
+GO
+
 CREATE TABLE Marca(
 MarcaID int Identity not null primary key,
 Nombre varchar(30),
 )
 GO
 
-ALTER TABLE Boleta
-add constraint fk_Boleta_cliente
+CREATE TABLE Distrito(
+DistritoID int Identity(1,1) not null primary key,
+NombreDist varchar(50),
+)
+GO
+
+
+CREATE TABLE Empleado(
+EmpleadoID int Identity(1,1) not null primary key,
+TipoEmpleadoID int not null,
+Nombre varchar(50) not null,
+ApellidoPaterno varchar(50) not null,
+ApellidoMaterno varchar(50) not null,
+Sueldo decimal(10,1) not null,
+Direccion varchar(150) not null,
+Telefono varchar(10) not null,
+Usuario char(5) not null unique,
+DistritoID int not null,
+constraint fk_TipoEmpleado_Empleado foreign key(TipoEmpleadoID) references TipoEmpleado(TipoEmpleadoID),
+constraint  fk_Distrito_Empleado foreign key(DistritoID) references Distrito(DistritoID),
+constraint  fk_Usuario_Empleado foreign key(Usuario) references Usuario(Usuario)
+)
+GO
+
+
+CREATE TABLE Cliente(
+ClienteID int Identity(1,1) not null primary key,
+Nombre varchar(50) not null,
+ApellidoPaterno varchar(50) not null,
+ApellidoMaterno varchar(50) not null,
+Direccion varchar(150) not null,
+Telefono varchar(10) not null,
+Usuario char(5) not null unique,
+TipoDocumentoID int not null,
+DistritoID int not null,
+constraint  fk_Distrito_cliente foreign key(DistritoID) references Distrito(DistritoID),
+constraint  fk_TipoDocumento_cliente foreign key(TipoDocumentoID) references TipoDocumento(TipoDocumentoID),
+constraint  fk_Usuario_Cliente foreign key(Usuario) references Usuario(Usuario)
+)
+GO
+
+CREATE TABLE Boleta(
+BoletaID int Identity(1,1) not null primary key,
+EmpleadoID int not null,
+ClienteID int not null,
+constraint fk_Boleta_cliente
 foreign key(EmpleadoID)
-references Empleado(EmpleadoID)
-GO
-ALTER TABLE Empleado
-add constraint fk_TipoEmpleado_Empleado
-foreign key(TipoEmpleadoID)
-references TipoEmpleado(TipoEmpleadoID)
-GO
-
-ALTER TABLE Cliente
-add constraint  fk_Distrito_cliente
-foreign key(DistritoID)
-references Distrito(DistritoID)
-GO
-
-ALTER TABLE Cliente
-add constraint  fk_TipoDocumento_cliente
-foreign key(TipoDocumentoID)
-references TipoDocumento(TipoDocumentoID)
-GO
-
-ALTER TABLE Empleado
-add constraint  fk_Distrito_Empleado
-foreign key(DistritoID)
-references Distrito(DistritoID)
-GO
-
-ALTER TABLE Boleta
-add constraint  fk_Cliente_Boleta
+references Empleado(EmpleadoID),
+constraint  fk_Cliente_Boleta
 foreign key(ClienteID)
 references Cliente(ClienteID)
+)
 GO
 
-ALTER TABLE Producto
-add constraint fk_Producto_tipo
+CREATE TABLE Producto(
+ProductoID int Identity(1,1) not null primary key,
+Nombre varchar(30) not null,
+MarcaID int not null,
+Precio decimal(5,2) not null,
+Stock int not null,
+Foto image,
+TipoProdId int not null,
+constraint fk_Producto_tipo
 foreign key (TipoProdId)
-references TipoProducto(TipoProdId)
-GO
-
-ALTER TABLE Producto
-add constraint fk_Producto_Marca
+references TipoProducto(TipoProdId),
+constraint fk_Producto_Marca
 foreign key (MarcaID)
 references Marca(MarcaID)
+)
 GO
 
-
-ALTER TABLE DetalleBoleta
-add constraint fk_detalle_Boleta
+CREATE TABLE DetalleBoleta(
+ProductoID int not null,
+BoletaID int not null,
+IGV decimal(3,2) not null,
+Cantidad int not null,
+Precio decimal(5,2) not null,
+total decimal(9,2) not null,
+primary key (ProductoID,BoletaID),
+constraint fk_detalle_Boleta
 foreign key (BoletaID)
-references Boleta(BoletaID)
-GO
-
-ALTER TABLE DetalleBoleta
-add constraint fk_detalle_Producto
+references Boleta(BoletaID),
+constraint fk_detalle_Producto
 foreign key (ProductoID)
 references Producto(ProductoID)
+)
 GO
+--Admi
+insert into Usuario values ('U0001','Samu','123')
+insert into Usuario values ('U0002','Dani','234')
+insert into Usuario values ('U0003','CGloria','345')
+--clientes
+insert into Usuario values ('U0004','pJuan','456')
+insert into Usuario values ('U0005','JoseSala','567')
+insert into Usuario values ('U0006','SuperMari','678')
+insert into Usuario values ('U0007','HurMonica','789')
+insert into Usuario values ('U0008','','891')
+insert into Usuario values ('U0009','','912')
+insert into Usuario values ('U0010','','101')
+insert into Usuario values ('U0011','','102')
 
-INSERT INTO TipoEmpleado VALUES ('Administrador','123456789')
-GO
-INSERT INTO TipoEmpleado VALUES ('Vendedor','123456')
-GO
+
+
+INSERT INTO TipoEmpleado VALUES ('Administrador')
+INSERT INTO TipoEmpleado VALUES ('Administrador')
+INSERT INTO TipoEmpleado VALUES ('Vendedor')
+
+Select *  from TipoEmpleado
+
+
 INSERT INTO Distrito VALUES ('LIMA')
-GO
 INSERT INTO Distrito VALUES ('VMT')
-GO
 INSERT INTO Distrito VALUES ('VES')
-GO
 INSERT INTO Distrito VALUES ('SURCO')
-GO
 INSERT INTO Distrito VALUES ('SURQUILLO')
-GO
 INSERT INTO Distrito VALUES ('COMAS')
-GO
 INSERT INTO Distrito VALUES ('MIRAFLORES')
-GO
 INSERT INTO Distrito VALUES ('CHORRILLOS')
-GO
 INSERT INTO Distrito VALUES ('MAGDALENA')
-GO
+
+Select *  from Distrito
 
 INSERT INTO TipoProducto VALUES ('TP001','Oficina','asdasdsad')
-GO
 INSERT INTO TipoProducto VALUES ('TP002','Escolares','asdasdsad')
-GO
 INSERT INTO TipoProducto VALUES ('TP003','Arte','asdasdsad')
-GO
 INSERT INTO TipoProducto VALUES ('TP004','Computación','asdasdsad')
-GO
 INSERT INTO TipoProducto VALUES ('TP005','Electrónica','asdasdsad')
-GO
 INSERT INTO TipoProducto VALUES ('TP006','Manualidades','asdasdsad')
-GO
 INSERT INTO TipoProducto VALUES ('TP007','Regalería','asdasdsad')
-GO
+
+Select *  from TipoProducto
 
 INSERT INTO Empleado VALUES(1,'EMP01','Samuel','Cusihuallpa','Guillen',100.1,'Av. Simon Bolivar','2833029',1)
-GO
 INSERT INTO Empleado VALUES(2,'EMP02','Danilo','Zamudio','Arevalo XD!',2000,'Av. Lurin','4257821',3)
-GO
 
----PROCEDIMIENTOS ALMACENADOS
 
-USE Libreria
-GO 
+insert into Cliente values('C0001','Perez Quispe','Juan','AV.28 de Julio 168')
+insert into Cliente values('C0002','Salazar Magariño','Jose','AV.Los Faisanes 180')
+insert into Cliente values('C0003','Dominguez Rodriguez','Sabrina','AV.Guardia civil 171')
+insert into Cliente values('C0004','Alveda Villar','Mariela','AV.Huylas 120')
+insert into Cliente values('C0005','Hurtado Espinoza','Monica','AV.Las Gaviotas 145')
+insert into Cliente values('C0006','Rojas Molina','Oscar','AV.Los Manzanales 854')
+insert into Cliente values('C0007','Alcala Muñoz','Pedro','Jr.Los Palcos 192')
+insert into Cliente values('C0008','Zamudio Arriola','Mario','Jr.13 de Agosto 689')
 
-CREATE PROCEDURE [dbo].[InsertarBoleta]
-@EMPLEADOID INT,
-@CLIENTEID INT
-AS
-INSERT INTO Boleta(EmpleadoID,ClienteID)
-VALUES(@EMPLEADOID,@CLIENTEID)
-return @@identity
-GO
 
-CREATE PROCEDURE [dbo].[InsertarDistrito]
-@NOMBRE varchar(50)
-AS
-INSERT INTO Distrito(NombreDist)
-VALUES(@NOMBRE)
-GO
-
-CREATE PROCEDURE [dbo].[InsertarEmpleado]
-@TIPOEMPLEADOID INT,
-@CODIGOEMPLEADO VARCHAR(5),
-@NOMBRE VARCHAR(50),
-@APELLIDOPATERNO VARCHAR(50),
-@APELLIDOMATERNO VARCHAR(50),
-@SUELDO DECIMAL(10,1),
-@DIRECCION VARCHAR(150),
-@TELEFONO VARCHAR(10),
-@DISTRITOID INT
-AS
-INSERT INTO Empleado(TipoEmpleadoID,CodigoEmpleado,Nombre,ApellidoPaterno,Sueldo,ApellidoMaterno,direccion,telefono,DistritoID)
-VALUES(@TIPOEMPLEADOID,@CODIGOEMPLEADO,@NOMBRE,@APELLIDOPATERNO,@SUELDO,@APELLIDOMATERNO,@DIRECCION,@TELEFONO,@DISTRITOID)
-GO
-
-CREATE PROCEDURE [dbo].[InsertarCliente]
-@NOMBRE VARCHAR(50),
-@APELLIDOPATERNO VARCHAR(50),
-@APELLIDOMATERNO VARCHAR(50),
-@DIRECCION VARCHAR(150),
-@TELEFONO VARCHAR(10),
-@TIPODOCUMENTOID CHAR(11),
-@DISTRITOID INT
-AS
-INSERT INTO Cliente(Nombre,ApellidoPaterno,ApellidoMaterno,Direccion,Telefono,TipoDocumentoID,DistritoID)
-VALUES(@NOMBRE,@APELLIDOPATERNO,@APELLIDOMATERNO,@DIRECCION,@TELEFONO,@TIPODOCUMENTOID,@DISTRITOID)
-GO
-
-CREATE PROCEDURE [dbo].[InsertarTipoProducto]
-@CODIGO VARCHAR(5),
-@NOMBRE VARCHAR(30),
-@DECRIPCION VARCHAR(60)
-AS
-INSERT INTO TipoProducto(Codigo,Nombre,Descripcion)
-VALUES(@CODIGO,@NOMBRE,@DECRIPCION)
-GO
-
-CREATE PROCEDURE [dbo].[InsertarProducto]
-@NOMBRE VARCHAR(30),
-@MARCAID VARCHAR(30),
-@PRECIO DECIMAL(5,2),
-@STOCK INT,
-@FOTO IMAGE,
-@TIPOPRODID INT
-AS
-INSERT INTO Producto(Nombre,MarcaID,Precio,Stock,Foto,TipoProdID)
-VALUES(@NOMBRE,@MARCAID,@PRECIO,@STOCK,@FOTO,@TIPOPRODID)
-GO
-
-CREATE PROCEDURE [dbo].[InsertarDetalleBoleta]
-@PRODUCTOID INT,
-@BOLETAID INT,
-@IGV DECIMAL(3,2),
-@CANTIDAD INT,
-@PRECIO DECIMAL(5,2),
-@TOTAL DECIMAL(9,2)
-AS
-INSERT INTO DetalleBoleta(ProductoID,BoletaID,IGV,Cantidad,Precio,total)
-VALUES(@PRODUCTOID,@BOLETAID,@IGV,@CANTIDAD,@PRECIO,@TOTAL)
-GO
-
-CREATE PROCEDURE [dbo].[ListarEmpleados]
-AS
-SELECT EmpleadoID [ID],
-E.TipoEmpleadoID,''[TipoEmpleadoID],
-T.Descripcion [TipoEmpleado],
-ISNULL(E.CodigoEmpleado,'')[Codigo],
-ISNULL(E.Nombre ,'')[Nombre],
-ISNULL(ApellidoPaterno,'')[ApellidoPaterno],
-ISNULL(ApellidoMaterno,'')[ApellidoMaterno],
-ISNULL(Sueldo,'')[Sueldo],
-ISNULL(Direccion,'')[Direccion],
-ISNULL(Telefono,'')[Telefono],
-E.DistritoID,''[DistritoID],
-D.NombreDist [Distrito]
-FROM Empleado E INNER JOIN Distrito D
-ON E.DistritoID=D.DistritoID
-INNER JOIN TipoEmpleado T
-ON E.TipoEmpleadoID=T.TipoEmpleadoID
-GO
-
-CREATE PROCEDURE [dbo].[ListarCliente]
-AS 
-SELECT ClienteID [ID],
-ISNULL(C.Nombre,'')[Nombre],
-ISNULL(ApellidoPaterno,'')[ApellidoPaterno],
-ISNULL(ApellidoMaterno,'')[ApellidoMaterno],
-ISNULL(Direccion,'')[Direccion],
-ISNULL(Telefono,'')[Telefono],
-C.TipoDocumentoID,''[TipoDocumentoID],
-T.Descripcion [TipoDocumento],
-C.DistritoID,''[DictritoID],
-D.NombreDist [Distrito]
-FROM Cliente C INNER JOIN Distrito D
-ON C.DistritoID=D.DistritoID
-INNER JOIN TipoDocumento T
-ON C.TipoDocumentoID=T.TipoDocumentoID
-GO
-
-CREATE PROCEDURE [dbo].[ListarProducto]
-AS 
-SELECT ProductoID [ID],
-ISNULL(P.Nombre,'')[Nombre],
-P.MarcaID,''[MarcaID],
-M.Nombre[Marca],
-ISNULL(Precio,'')[Precio],
-ISNULL(Stock,'')[Stock],
-Foto,
-P.TipoProdID,''[TipoProdID],
-T.Descripcion[TipoProducto]
-FROM Producto P INNER JOIN TipoProducto T
-ON P.TipoProdId=T.TipoProdId
-INNER JOIN Marca M
-ON P.MarcaID=M.MarcaID
-GO
-
-CREATE PROCEDURE [dbo].[ListarTipoProducto]
-AS  
-SELECT  TipoProdID [ID],
-ISNULL(Codigo,'')[Codigo],
-ISNULL(Nombre,'')[Nombre],
-ISNULL(Descripcion,'')[Descripcion]
-FROM TipoProducto
-GO
-
-CREATE PROCEDURE [dbo].[ListarDistrito]
-AS  
-SELECT DistritoID [ID],
-ISNULL(NombreDist,'')[NombreDist]
-FROM  Distrito
-GO
-CREATE PROCEDURE [dbo].[ListarTipoEmpleado]
-AS  
-SELECT TipoEmpleadoID [ID],
-ISNULL(Descripcion,'')[Descripcion],
-ISNULL(Contrasena,'')[Contrasena]
-FROM  TipoEmpleado
-GO
-
-CREATE PROCEDURE [dbo].[login]
-@Descripcion VARCHAR(20),
-@Contrasena VARCHAR(20)
-AS
-SELECT * FROM TipoEmpleado 
-WHERE Descripcion =@Descripcion and Contrasena =@Contrasena 
-GO
-
-CREATE PROCEDURE [dbo].[ModificarProducto]
-@ID INT,
-@NOMBRE VARCHAR(30),
-@MARCAID VARCHAR(30),
-@PRECIO DECIMAL(5,2),
-@STOCK INT,
-@FOTO IMAGE,
-@TIPOPRODID INT
-AS
-UPDATE Producto SET Nombre=@NOMBRE,
-MarcaID=@MARCAID,
-Precio=@PRECIO,
-Stock=@STOCK,
-Foto=@FOTO,
-TipoProdId=@TIPOPRODID
-WHERE ProductoID=@ID
-GO
-
-CREATE PROCEDURE [dbo].[ModificarEmpleado]
-@ID INT,
-@TIPOEMPLEADOID INT,
-@CODIGOEMPLEADO VARCHAR(5),
-@NOMBRE VARCHAR(50),
-@APELLIDOPATERNO VARCHAR(50),
-@APELLIDOMATERNO VARCHAR(50),
-@SUELDO DECIMAL(10,1),
-@DIRECCION VARCHAR(150),
-@TELEFONO VARCHAR(10),
-@DISTRITOID INT
-AS
-UPDATE Empleado SET TipoEmpleadoID=@TIPOEMPLEADOID,
-CodigoEmpleado=@CODIGOEMPLEADO,
-Nombre=@NOMBRE,
-ApellidoPaterno=@APELLIDOPATERNO,
-ApellidoMaterno=@APELLIDOMATERNO,
-Sueldo = @SUELDO,
-Direccion=@DIRECCION,
-Telefono=@TELEFONO,
-DistritoID=@DISTRITOID
-WHERE EmpleadoID=@ID
-GO
---************************************************************************
-CREATE PROCEDURE [dbo].[ModificarDistrito]
-@ID INT,
-@NOMBRE VARCHAR(50)
-AS
-UPDATE Distrito SET NombreDist = @NOMBRE
-WHERE DistritoID = @ID
-GO
-
---************************************************************************
-
-CREATE PROCEDURE [dbo].[ModificarCliente]
-@ID INT,
-@NOMBRE VARCHAR(50),
-@APELLIDOPATERNO VARCHAR(50),
-@APELLIDOMATERNO VARCHAR(50),
-@DIRECCION VARCHAR(150),
-@TELEFONO VARCHAR(10),
-@TIPODOCUMENTOID CHAR(11),
-@DISTRITOID INT
-AS
-UPDATE Cliente SET Nombre=@NOMBRE,
-ApellidoPaterno=@APELLIDOPATERNO,
-ApellidoMaterno=@APELLIDOMATERNO,
-Direccion=@DIRECCION,
-Telefono=@TELEFONO,
-TipoDocumentoID=@TIPODOCUMENTOID,
-DistritoID=@DISTRITOID
-WHERE ClienteID=@ID
-GO
--------------------------------------------------------------------------------------------------
--------------------------------------------------------------------------------------------------
-CREATE PROCEDURE [dbo].[EliminarEmpleado]
-@ID INT
-AS
-DELETE FROM Empleado
-WHERE EmpleadoID = @ID
-GO
-------------------------------------------
-CREATE PROCEDURE [dbo].[EliminarCliente]
-@ID INT
-AS
-DELETE FROM Cliente
-WHERE ClienteID = @ID
-
-GO
-------------------------------------------
-CREATE PROCEDURE [dbo].[EliminarProducto]
-@ID INT
-AS
-DELETE FROM Producto
-WHERE ProductoID = @ID
-GO
-------------------------------------------
-
-CREATE PROCEDURE [dbo].[EliminarDistrito]
-@ID INT
-AS 
-DELETE FROM Distrito
-WHERE DistritoID = @ID
-GO
-------------------------------------------
+SELECT * FROM Cliente
