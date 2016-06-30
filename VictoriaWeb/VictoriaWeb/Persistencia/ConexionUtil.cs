8212bf1f795cc7a911200c16e447b041a917c55e
SCusihuallpaG <s_libratlv@hotmail.com>
2016-06-30 04:13:27 -0500

    Persisitencia Se fijan su conexionUtil

---
+++ VictoriaWeb/VictoriaWeb/Persistencia/ConexionUtil.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace VictoriaWeb.Persistencia
+{
+    public class ConexionUtil
+    {
+        public static string ObtenerCadena()
+        {
+            return "server=(local);database=Libreria; Integrated Security =True";
+        }
+    }
+}
