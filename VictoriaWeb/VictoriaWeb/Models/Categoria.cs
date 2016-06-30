157a9a275be895f2e31809a7543458f3209de04d
SCusihuallpaG <s_libratlv@hotmail.com>
2016-06-30 03:11:43 -0500

    models completado...Bueno si a gloria no se ocurreo seguir modificando la BD XD!!

---
+++ VictoriaWeb/VictoriaWeb/Models/Categoria.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace VictoriaWeb.Models
+{
+    public class Categoria
+    {
+        public string CategoriaID { get; set; }
+        public string Nombre { get; set; }
+    }
+}
---
+++ VictoriaWeb/VictoriaWeb/Models/SubCategoria.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace VictoriaWeb.Models
+{
+    public class SubCategoria
+    {
+        public string SubCatID { get; set; }
+        public string Nombre { get; set; }
+        public Categoria CategoriaID { get; set; }
+    }
+}
